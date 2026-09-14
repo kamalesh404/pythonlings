161153e34060a62618686911d042d68f0e9c55eb
Kamalesh <kamalesh2017d@gmail.com>
2026-09-14 21:06:10 +0530

    fix: add assertion message to itertools3.py

--- checks/itertools/itertools3.py
+++ checks/itertools/itertools3.py
@@ -1,2 +1,2 @@
-assert coordinates == [(0, "low"), (0, "high"), (1, "low"), (1, "high")]
+assert coordinates == [(0, "low"), (0, "high"), (1, "low"), (1, "high")], f"Expected [(0, 'low'), (0, 'high'), (1, 'low'), (1, 'high')], got {coordinates}"
 print("itertools3 ok")
